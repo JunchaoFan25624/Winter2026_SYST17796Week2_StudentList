26c8a8ec58dba365a5d0fddfc3e3c22c3fdd31af
JunchaoFan25624 <fanjun@sheridancollege.ca>
2026-01-12 17:09:05 -0500

    studentlist week2 inclass practise

---
+++ src/studentlist/Student.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/** sTUDENT ENTITY
+ *
+ * @author fjc27
+ */
+public class Student {
+    private String Name; // encapsulation principle private + getters, setters
+    private int sid;
+    //getters and setters
+
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fjc27
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // definr and print 3 student info name, sid using array
+        Student[] list = new Student[3];//array of obj // peter 1
+        Student s1 = new Student();// s1 obj
+        
+        s1.setName("Peter");
+        s1.setSid(1);
+        // store s1 obj in array
+        Student s2 = new Student();// s2 obj
+        s2.setName("John");
+        s2.setSid(2);
+        Student s3 = new Student();// s3 obj
+        s3.setName("Khan");
+        s3.setSid(3);
+        
+        list[0]=s1;
+        list[1]=s2;
+        list[2]=s3; //stores obj in array
+        
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i].getName() + " " + list[i].getSid());
+        }
+    }
+    
+}
