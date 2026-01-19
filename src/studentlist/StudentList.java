bc2e583a8cefc73163ea69e44c1cb9cdc026a4b8
JunchaoFan25624 <fanjun@sheridancollege.ca>
2026-01-19 16:42:56 -0500

    change

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -25,18 +25,19 @@ public class StudentList {
         Student s2 = new Student();// s2 obj
         s2.setName("John");
         s2.setSid(2);
-        // Student s3 = new Student();// s3 obj
-        // s3.setName("Khan");
-        // s3.setSid(3);
+         Student s3 = new Student();// s3 obj
+         s3.setName("Khan");
+         s3.setSid(3);
         
         list[0]=s1;
         list[1]=s2;
-        // list[2]=s3; //stores obj in array
+         list[2]=s3; //stores obj in array
         
         for (int i = 0; i < list.length; i++) {
             System.out.println(list[i].getName() + " " + list[i].getSid());
-        System.out.println("Finished") // fetch + merge
+         // fetch + merge
         }
+        System.out.println("Finished");
     }
     
 }
